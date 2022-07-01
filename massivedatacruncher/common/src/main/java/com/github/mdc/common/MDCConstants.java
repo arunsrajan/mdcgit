8b2063d044934bbcc8b918350e52c4cfd8fa11d9
arun <arunsrajan@gmail.com>
2022-07-02 02:58:01 +0530

    Color configurable for UI

--- massivedatacruncher/common/src/main/java/com/github/mdc/common/MDCConstants.java
+++ massivedatacruncher/common/src/main/java/com/github/mdc/common/MDCConstants.java
@@ -414,5 +414,9 @@ public class MDCConstants {
 	public static final String IMPLICIT_CONTAINER_ALLOC_MEMORY_SIZE = "containers.alloc.implicit.memory.size";
 	public static final String IMPLICIT_CONTAINER_ALLOC_MEMORY_SIZE_DEFAULT = "1";
 	
+	public static final String COLOR_PICKER_PRIMARY = "ui.color.primary";
+	public static final String COLOR_PICKER_PRIMARY_DEFAULT = "#6E5CDB";
+	public static final String COLOR_PICKER_ALTERNATE = "ui.color.alternate";
+	public static final String COLOR_PICKER_ALTERNATE_DEFAULT = "#ddddddd";
 	
 }
